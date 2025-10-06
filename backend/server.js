52f77b1d58f6651d534fab8995e11d0c12a7d0f1
UtkarshUmap <umapus@iitbhilai.ac.in>
2025-10-06 10:25:39 +0530

    Update server.js
    Added route in index.router.js

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,6 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import apiRouter from "./routes/index.router.js";
-import applicationRoutes from './routes/application.routes.js';
-import jobRoutes from './routes/jobs.routes.js';
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -26,11 +24,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // Use all API routes
 app.use("/api", apiRouter);
-app.use("/api/applications", applicationRoutes);
-app.use("/api/jobs", jobRoutes); 
 
 // Start the server
 app.listen(port, () => {
   connectDB();
   console.log(`Server is running at port ${port}`);
 });
+
